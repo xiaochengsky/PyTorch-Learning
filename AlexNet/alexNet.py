25552eda12e1120990266824734d7c012a8eaf20
xiaochengsky <626576619@qq.com>
2020-02-21 21:08:33 +0800

    AlexNet网络实现

---
+++ AlexNet/alexNet.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn, optim
+import torchvision
+import time
+
+import sys
+
+
+class AlexNet(nn.Module):
+    def __init__(self):
+        super(AlexNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 96, 11, 4),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+
+            nn.Conv2d(96, 256, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+
+            nn.Conv2d(256, 384, 3, 1, 1),
+            nn.Conv2d(384, 384, 3, 1, 1),
+            nn.Conv2d(384, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(256*5*5, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, img):
+        feature = self.conv(img)
+        output = self.fc(feature.view(img.shape[0], -1))
+        return output
+
+net = AlexNet()
+print(net)
