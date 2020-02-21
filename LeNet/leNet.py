56093c6edc1f21d58590dc03787fc923e997c63b
xiaochengsky <626576619@qq.com>
2020-02-21 20:24:05 +0800

    LeNet网络实现

--- LeNet/leNet.py
+++ LeNet/leNet.py
@@ -3,16 +3,75 @@ import torchvision
 from torchvision import transforms
 import time
 from torch import nn, optim
-
 import logger
+import sys
 
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# 下载数据集 FashionMNIST 数据集
-# mnist_train = torchvision.datasets.FashionMNIST(root='/mnt_datas/ycc/dataset/FashionMNIST', train=True, download=True, transform=transforms.ToTensor())
-# mnist_test = torchvision.datasets.FashionMNIST(root='/mnt_datas/ycc/dataset/FashionMNIST', train=False, download=True, transform=transforms.ToTensor())
-# print(type(mnist_train))
-# print(len(mnist_train), len(mnist_test))
+def load_data_fashion_mnist(batch_size, resize=None, root='/mnt_datas/ycc/dataset/FashionMNIST'):
+    """Download the fashion mnist dataset and then load into memory."""
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+
+    transform = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
+
+
+def evaluate_accuracy(data_iter, net, device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
+    acc_sum, n = 0.0, 0
+    with torch.no_grad:
+        for X, y in data_iter:
+            # 验证模式, 放弃 dropOut
+            net.eval()
+            acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu.item()
+            # 改为训练模式
+            net.train()
+            n += y.shape[0]
+    return acc_sum / n
+
+
+def train_leNet(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on", device)
+    loss = torch.nn.CrossEntropyLoss()
+    batch_count = 0
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            i = 1
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            if i == 1:
+                logger('loss: ' + loss)
+
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            if i == 1:
+                logger('y_hat: ' + y_hat)
+                logger('y_hat.shape: ' + y_hat.shape)
+                logger('y_hat.argmax(dim=1): ' + y_hat.argmax(dim=1))
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, traiin acc %.3f, test acc %.3f, time %.1f sec' %
+              (epoch + 1), train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start)
+
 
 class LeNet(nn.Module):
     def __init__(self):
@@ -45,5 +104,12 @@ net = LeNet()
 # 查看网络各层大小
 log.info(net)
 
+
+# 获取数据和训练数据
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+train_leNet(net, train_iter, test_iter, batch_size, optimizer, device=device, num_epochs)
 
 
