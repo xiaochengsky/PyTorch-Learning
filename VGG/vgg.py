4cd3c0c6dc03c5d58feb78ae2c0aa5ac7ff2399d
xiaochengsky <626576619@qq.com>
2020-02-21 23:01:36 +0800

    VGG网络实现

--- VGG/vgg.py
+++ VGG/vgg.py
@@ -1,8 +1,70 @@
 import torch
 from torch import nn, optim
+import torchvision
 import time
 
 import sys
+
+
+# 加载 FashionMNIST 数据集
+def load_data_fashion_mnist(batch_size, resize=None, root='/mnt_datas/ycc/dataset/FashionMNIST'):
+    """Download the fashion mnist dataset and then load into memory."""
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+
+    transform = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
+
+
+
+# 验证当前网络在测试集的精确度
+def evaluate_accuracy(data_iter, net, device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
+    acc_sum, n = 0.0, 0
+    for X, y in data_iter:
+        # 验证模式, 放弃 dropOut
+        net.eval()
+        acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+        # 改为训练模式
+        net.train()
+        n += y.shape[0]
+    return acc_sum / n
+
+
+# AlexNet网络
+def train_alexNet(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on", device)
+    loss = torch.nn.CrossEntropyLoss()
+    batch_count = 0
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            i = 1
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec' %
+              (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
 
 
 #
@@ -32,7 +94,7 @@ def vgg(conv_arch, fc_features, fc_hidden_uints=4096):
     net = nn.Sequential()
     for i, (num_convs, in_channels, out_channels) in enumerate(conv_arch):
         net.add_module("vgg_block_" + str(i + 1), vgg_block(num_convs, in_channels, out_channels))
-    net.add_module("fc", nn.Sequential(
+    net.add_module("fc", nn.Sequential(FlattenLayer(),
                                        nn.Linear(fc_features, fc_hidden_uints), nn.ReLU(), nn.Dropout(0.5),
                                        nn.Linear(fc_hidden_uints, fc_hidden_uints), nn.ReLU(), nn.Dropout(0.5),
                                        nn.Linear(fc_hidden_uints, 10)
@@ -44,11 +106,24 @@ def vgg(conv_arch, fc_features, fc_hidden_uints=4096):
 conv_arch = ((1, 1, 64), (1, 64, 128), (2, 128, 256), (2, 256, 512), (2, 512, 512))
 fc_features = 512 * 7 * 7
 fc_hidden_uints = 4096
-net = vgg(conv_arch, fc_features, fc_hidden_uints)
+# net = vgg(conv_arch, fc_features, fc_hidden_uints)
 
 # 测试数据
-X = torch.rand(1, 1, 224, 224)
+# X = torch.rand(1, 1, 224, 224)
+#
+# for name, blk in net.named_children():
+#     X = blk(X)
+#     print(name, 'output shape: ', X.shape)
 
-for name, blk in net.named_children():
-    X = blk(X)
-    print(name, 'output shape: ', X.shape)
+ratio = 8
+small_conv_arch = ((1, 1, 64//ratio), (1, 64//ratio, 128//ratio), (2, 128//ratio, 256//ratio),
+                   (2, 256//ratio, 512//ratio), (2, 512//ratio, 512//ratio))
+net = vgg(small_conv_arch, fc_features//ratio, fc_hidden_uints//ratio)
+print(net)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+batch_size = 64
+train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224)
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+train_iter(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
