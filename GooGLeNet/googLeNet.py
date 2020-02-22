d280e38a003231d68e354d82242f0c204366c792
xiaochengsky <626576619@qq.com>
2020-02-22 14:21:54 +0800

    GoogLeNet网络实现

--- GooGLeNet/googLeNet.py
+++ GooGLeNet/googLeNet.py
@@ -30,10 +30,10 @@ class Inception(nn.Module):
         self.p1_1 = nn.Conv2d(in_c, c1, kernel_size=1)
         # line2
         self.p2_1 = nn.Conv2d(in_c, c2[0], kernel_size=1)
-        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
         # line3
         self.p3_1 = nn.Conv2d(in_c, c3[0], kernel_size=1)
-        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
         # line4
         self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         self.p4_2 = nn.Conv2d(in_c, c4, kernel_size=1)
@@ -43,7 +43,7 @@ class Inception(nn.Module):
         p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
         p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
         p4 = F.relu(self.p4_2(self.p4_1(x)))
-        return torch.cat(p1, p2, p3, p4, dim=1)
+        return torch.cat((p1, p2, p3, p4), dim=1)
 
 
 b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
@@ -78,3 +78,8 @@ for blk in net.children():
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+
+
